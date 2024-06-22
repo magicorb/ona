edae8f68d34937e51b464f2a90d2b30c04ec30cf
ForgeDimaTashkinov <dima.tashkinov@forgeglobal.com>
2024-06-22 13:25:40 +0100

    Month view

--- Ona.App/Calendar/MonthViewModel.cs
+++ Ona.App/Calendar/MonthViewModel.cs
@@ -1,25 +1,54 @@
-﻿using Ona.App.Mvvm;
+﻿using Ona.App.Model;
+using Ona.App.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ona.App.Model;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ona.App.Calendar
 {
 	public class MonthViewModel : ViewModelBase
 	{
+		private readonly IDateTimeProvider dateTimeProvider;
+
 		private readonly int year;
 		private readonly int month;
 
-		public MonthViewModel(int year, int month)
+		public MonthViewModel(
+			IDateTimeProvider dateTimeProvider,
+			int year,
+			int month)
 		{
+			this.dateTimeProvider = dateTimeProvider;
 			this.year = year;
 			this.month = month;
+
+			DaysOfWeek = CultureInfo.CurrentUICulture.DateTimeFormat.AbbreviatedDayNames;
+
+			Dates = GenerateDates();
 		}
 
 		public string MonthName
-			=> new DateTime(this.year, this.month, 1).ToString("MMMM", CultureInfo.CreateSpecificCulture("en-us"));
+			=> MonthStart.ToString("MMMM", CultureInfo.CreateSpecificCulture("en-us"));
+
+		public IEnumerable<string> DaysOfWeek { get; }
+
+		public IEnumerable<DateViewModel> Dates { get; }
+
+		private IEnumerable<DateViewModel> GenerateDates()
+		{
+			var firstDate = MonthStart.StartOfWeek(DayOfWeek.Monday);
+
+			return firstDate.DateRange(firstDate.AddMonths(1).AddDays(-1))
+				.Select(d => new DateViewModel(d, this.dateTimeProvider))
+				.ToArray();
+		}
+
+		private DateTime MonthStart
+			=> new DateTime(this.year, this.month, 1);
 	}
 }
