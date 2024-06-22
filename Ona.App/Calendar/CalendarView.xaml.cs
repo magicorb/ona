66ccfa7a2d2caa95a4d129070ffa29e60bac3aea
ForgeDimaTashkinov <dima.tashkinov@forgeglobal.com>
2024-06-22 20:59:39 +0100

    Reduce scrolling errors

--- Ona.App/Calendar/CalendarView.xaml.cs
+++ Ona.App/Calendar/CalendarView.xaml.cs
@@ -14,28 +14,40 @@ public partial class CalendarView : ContentView
 		BindingContext = this.viewModel = new CalendarViewModel(new DateTimeProvider());
 
 		this.isLoading = true;
-		Dispatcher.DispatchDelayed(TimeSpan.FromSeconds(2), () => this.isLoading = false);
+		Dispatcher.DispatchDelayed(TimeSpan.FromSeconds(4), () => this.isLoading = false);
 	}
 
 	private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
 	{
 		var collectionView = (CollectionView)sender;
 
-		if (e.LastVisibleItemIndex == this.viewModel.Months.Count - 1)
+		if (e.LastVisibleItemIndex >= this.viewModel.Months.Count - 2)
+			AppendMonth();
+		else if (e.FirstVisibleItemIndex <= 1)
+			InsertMonth();
+	}
+
+	private void AppendMonth()
+	{
+		Dispatcher.Dispatch(() =>
 		{
 			if (this.isLoading)
 				return;
 			this.isLoading = true;
 			this.viewModel.AppendMonth();
 			this.isLoading = false;
-		}
-		else if (e.FirstVisibleItemIndex == 0)
+		});
+	}
+
+	private void InsertMonth()
+	{
+		Dispatcher.Dispatch(() =>
 		{
 			if (this.isLoading)
 				return;
 			this.isLoading = true;
 			this.viewModel.InsertMonth();
 			this.isLoading = false;
-		}
+		});
 	}
 }
