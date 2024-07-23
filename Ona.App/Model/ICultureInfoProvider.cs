3bb4839d24845568b2d342aa06945f6cb292173a
ForgeDimaTashkinov <dima.tashkinov@forgeglobal.com>
2024-07-23 22:30:50 +0100

    Fix days of week for non US cultures

---
+++ Ona.App/Model/ICultureInfoProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ona.App.Model
+{
+    public interface ICultureInfoProvider
+    {
+        CultureInfo CurrentUICulture { get; }
+    }
+}
