74b31fa5c6109aef2dfb0bc51e053e7f2d3ed8e2
natnaumova <67557328+natnaumova@users.noreply.github.com>
2024-08-03 16:44:39 +0100

    Widget refactoring, period state draft

--- Ona.Widget/ona/ContentView.swift
+++ Ona.Widget/ona/ContentView.swift
@@ -9,19 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var count = UserDefaults(suiteName: "group.com.natalianaumova.ona")!.integer(forKey: "Count");
+    //@Binding private var periodState: PeriodState
+    //@Binding private var start: String
+    //@Binding private var duration: String
+    //@Binding private var interval: String
+    
     var body: some View {
-        VStack {
+        //periodState = getPeriodState()
+        //start = periodState.start
+        //duration = "\(periodState.duration)"
+        //interval = "\(periodState.interval)"
+
+        return VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Count: \(count)")
-            Button("Increment") {
-                count += 1;
-                UserDefaults(suiteName: "group.com.natalianaumova.ona")!.set(count, forKey: "Count");
+            HStack {
+                Text("Count: \(count)")
+                Button("Increment") {
+                    count += 1;
+                    UserDefaults(suiteName: "group.com.natalianaumova.ona")!.set(count, forKey: "Count");
+                }
             }
+            //TextField("Start", text: $start)
+            //TextField("Duration", text: $duration)
+            //TextField("Interval", text: $interval)
         }
         .padding()
     }
+    
+    func getPeriodState() -> PeriodState {
+        let fallback = PeriodState(start: "N/A", duration: 0, interval: 0)
+        
+        guard let userDefaults = UserDefaults(suiteName: "group.com.natalianaumova.ona") else {
+            return fallback
+        }
+        return userDefaults.object(forKey: "PeriodState") as? PeriodState ?? fallback
+    }
+}
+
+struct PeriodState {
+    let start: String
+    let duration: Int
+    let interval: Int
 }
 
 #Preview {
