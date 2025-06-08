95a6b30950ede2da0074f76ffdd9ccbedbe20f31
Dmitry Tashkinov <DTashkinov@gmail.com>
2025-06-08 11:19:42 +0100

    Scrolling

--- Ona.App/MainPage.xaml.cs
+++ Ona.App/MainPage.xaml.cs
@@ -9,17 +9,9 @@
 			InitializeComponent();
 		}
 
-		//private void OnCounterClicked(object sender, EventArgs e)
-		//{
-		//	count++;
-
-		//	if (count == 1)
-		//		CounterBtn.Text = $"Clicked {count} time";
-		//	else
-		//		CounterBtn.Text = $"Clicked {count} times";
-
-		//	SemanticScreenReader.Announce(CounterBtn.Text);
-		//}
+		private void CalendarView_SizeChanged(object sender, EventArgs e)
+		{
+			_ = ScrollView.ScrollToAsync(CalendarView, ScrollToPosition.End, false);
+		}
 	}
-
 }
