f572a9966944075dd5897515089419b90bb291f1
Dmitry Tashkinov <DTashkinov@gmail.com>
2025-06-08 11:20:00 +0100

    Fix expected duration

--- Ona.App/Model/DateTimePeriod.cs
+++ Ona.App/Model/DateTimePeriod.cs
@@ -11,9 +11,18 @@ namespace Ona.App.Model
 		public DateTime Start { get; set; }
 
 		public DateTime End { get; set; }
+	}
 
-		public TimeSpan Length => End - Start;
+	public static class DateTimePeriodExtensions
+	{
+		public static TimeSpan Length(this DateTimePeriod period) => period.End - period.Start;
 
-		public int Days => (End - Start).Days + 1;
+		public static int DayCount(this DateTimePeriod period) => (period.End - period.Start).Days + 1;
+
+		public static IEnumerable<DateTime> Dates(this DateTimePeriod period)
+		{
+			for (var date = period.Start; date <= period.End; date = date.AddDays(1))
+				yield return date;
+		}
 	}
 }
