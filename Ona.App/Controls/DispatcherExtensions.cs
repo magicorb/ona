f302df950772c9bd07f10684e1137753e0380a0e
Dmitry Tashkinov <DTashkinov@gmail.com>
2025-06-08 11:19:53 +0100

    Show spinner only when loading

---
+++ Ona.App/Controls/DispatcherExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ona.App.Controls
+{
+	public static class DispatcherExtensions
+	{
+		public static Task DoEventsAsync(this IDispatcher dispatcher)
+		{
+			var tcs = new TaskCompletionSource();
+			dispatcher.Dispatch(tcs.SetResult);
+			return tcs.Task;
+		}
+	}
+}
