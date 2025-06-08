1fd7c8f316abd0073002bd39c11ef92e4eddbe94
Dmitry Tashkinov <DTashkinov@gmail.com>
2025-06-08 11:19:56 +0100

    iPhone widget draft

---
+++ Ona.Widget/counter/counterBundle.swift
@@ -0,0 +1,16 @@
+//
+//  counterBundle.swift
+//  counter
+//
+//  Created by Natalia Naumova on 29/07/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct counterBundle: WidgetBundle {
+    var body: some Widget {
+        counter()
+    }
+}
